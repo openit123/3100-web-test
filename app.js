1d3e355efdf37e838e727089b6427c02ddc6d606
matthew <1155083279@link.cuhk.edu.hk>
2018-03-18 23:10:04 +0800

    update : kelvin

--- app.js
+++ app.js
@@ -1,56 +1,83 @@
-var express = require('express');
+var express = require('express'); //MUST USE
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');// for cookies
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');// for handling session
+var passport = require('passport'); // for login function
+var LocalStrategy = require('passport-local').Strategy; // for local connection, can swtich to http
+var mongo = require('mongodb'); // database: Mongo DB
+var mongoose = require('mongoose'); // API between nodejs and mongo
+
+mongoose.connect('mongodb://localhost/loginapp'); // can modify the last items 'abc'
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
 var app = express();
 
-// db setup
-mongoose.connect('mongodb://localhost/csci3100');
-
-// view engine setup
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.render('index', {title: 'Home Page'});
-});
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
 
-app.get('/login', function (req, res) {
-    res.render('login');
-});
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/register',function(req,res){
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-});
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
-// catch 404 and forward to error handler
+// Connect Flash
+app.use(flash());
+
+// Global Vars
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/', routes);
+app.use('/users', users);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+// Set Port to :3000
+app.listen(3000, function(){
+	console.log('Server started on port 3000');
 });
-
-module.exports = app;
