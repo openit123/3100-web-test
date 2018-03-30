b802850124437aea5dbe5878f42b1abbc909e923
AlvinLuk <1155086091@link.cuhk.edu.hk>
2018-03-31 02:12:21 +0800

    Db now up. Let's play matching!

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongojs = require('mongojs');
+var db = mongojs('myfirst', ['pets']);
+var MongoClient = require('mongodb').MongoClient,format = require('util').format;
+MongoClient.connect('mongodb://127.0.0.1:27017', function(err,db){
+    if(err){
+        throw err;
+    }else{
+        console.log("connected");
+    }
+    db.close();
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +30,14 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+    res.locals.errors = null;
+    res.locals.types_of_pet = null;
+    res.locals.gender = null;
+    res.locals.years_old = null;
+    next();
+});
+
 app.get('/', function (req, res) {
     res.render('index.ejs');
 })
@@ -31,9 +50,31 @@ app.get('/profile', function (req, res){
     res.render('profile.ejs');
 })
 
-app.get('/search', function (req, res) {
-    res.render('search.ejs');
-})
+app.get('/search', function(req, res, data){
+    db.pets.find(function (err, docs){
+        console.log(docs);
+        res.render('search.ejs', {
+            pets: docs,
+        });
+    })
+});
+
+app.post('/search/pets/add', function(req, res){
+    var search ={
+        types_of_pet: req.body.types_of_pet,
+        gender: req.body.gender,
+        years_old: req.body.years_old,
+    }
+    db.pets.find(function (err, docs){
+        console.log(docs);
+        res.render('search.ejs', {
+            pets: docs,
+            types_of_pet: search.types_of_pet,
+            gender: search.gender,
+            years_old: search.years_old
+        });
+    })
+});
 
 app.get('/signin',function (req,res){
     res.render('signin.ejs');
