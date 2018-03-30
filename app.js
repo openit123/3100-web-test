2e33fd7f758c03210a20849dadcb90a08f13ddf1
matthew <1155083279@link.cuhk.edu.hk>
2018-03-30 16:34:55 +0800

    Merge: index add

--- app.js
+++ app.js
@@ -1,86 +1,54 @@
-var express = require('express'); //MUST USE
+var createError = require('http-errors');
+var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');// for cookies
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');// for handling session
-var passport = require('passport'); // for login function
-var LocalStrategy = require('passport-local').Strategy; // for local connection, can swtich to http
-var mongo = require('mongodb'); // database: Mongo DB
-var mongoose = require('mongoose'); // API between nodejs and mongo
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-mongoose.connect('mongodb://localhost/loginapp'); // can modify the last items 'abc'
-var db = mongoose.connection;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Init App
 var app = express();
 
-// View Engine
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// BodyParser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-
-// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+})
 
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
+app.get('/feedback', function (req, res) {
+    res.render('feedback.ejs');
+})
 
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.get('/search', function (req, res) {
+    res.render('search.ejs');
+})
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-app.use(flash());
-app.get('/feedback', function(req, res) {
-    res.render('trash_feedback/trash', {title:'Feedback'});
-});
-
-app.get('/feedback/text', function(req, res) {
-    let input = req.query.text;
-    res.json({result: input});
-});
+app.get('/signin',function (req,res){
+    res.render('signin.ejs');
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
+    next(createError(404));
 });
 
-app.use('/', routes);
-app.use('/users', users);
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Set Port to :3000
-app.listen(3000, function(){
-	console.log('Server started on port 3000');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+module.exports = app;
